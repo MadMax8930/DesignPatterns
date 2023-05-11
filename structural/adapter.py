287fd5b0c04a842754ba055e2edc1f5f7113319e
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-05-11 15:47:28 +0200

    usb adapter

--- structural/adapter.py
+++ structural/adapter.py
@@ -1,1 +1,44 @@
 #Structural Pattern
+
+class UsbCable:
+   def __init__(self):
+      self.isPlugged = False
+      
+   def plugUsb(self):
+      self.isPlugged = True
+      
+class UsbPort:
+   def __init__(self):
+      self.portAvailable = True
+   
+   def plug(self, usb):
+      if self.portAvailable:
+         usb.plugUsb()
+         self.portAvailable = False
+    
+# UsbCables can plug directly into Usb ports
+usbCable = UsbCable()
+usbPort1 = UsbPort()
+usbPort1.plug(usbCable)
+print("usbCable is plugged:", usbCable.isPlugged)
+
+class MicroUsbCable:
+   def __init__(self):
+      self.isPlugged = False
+   
+   def plugMicroUsb(self):
+      self.isPlugged = True
+      
+class MicroToUsbAdapter(UsbCable):
+   def __init__(self, microUsbCable):
+      self.microUsbCable = microUsbCable
+      self.microUsbCable.plugMicroUsb()
+      
+   # Can override UsbCable.plugUsb() if needed
+   
+# MicroUsbCables can plug into Usb ports via an adapter
+microToUsbAdapter = MicroToUsbAdapter(MicroUsbCable())
+
+usbPort2 = UsbPort()
+usbPort2.plug(microToUsbAdapter)
+print("microToUsbAdapter is plugged:", microToUsbAdapter.isPlugged)
